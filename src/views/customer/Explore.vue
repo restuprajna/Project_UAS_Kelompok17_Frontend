7ef5ca92187115f19e795d42916673f7a0d148a6
purnamaadi <purnama.adi2001@gmail.com>
2022-01-12 23:19:41 +0800

    update explore and home

--- src/views/customer/Explore.vue
+++ src/views/customer/Explore.vue
@@ -1,90 +1,90 @@
 <template>
-    <div>
-        <Navbar />
+  <div>
+    <Navbar />
 
-        <!-- Banner start here -->
-        <div class="container-fluid banner ">
+    <!-- Banner start here -->
 
-            <div>
-                <h2><div class="text-center mt-5"><strong>Find Your Car Now</strong></div></h2>
-                <h2><div class="text-center ">Lets find your ideal car at Bali best rent car</div></h2>
-            </div>
+    <div class="container-fluid banner">
+      <div>
+        <h2>
+          <div class="text-center mt-5"><strong>Find Your Car Now</strong></div>
+        </h2>
+        <h2>
+          <div class="text-center">
+            Lets find your ideal car at Bali best rent car
+          </div>
+        </h2>
+      </div>
 
-            <img src="../../assets/img/home-img.png" class="rounded mx-auto d-block pt-5" alt="">
+      <img
+        src="../../assets/img/home-img.png"
+        class="rounded mx-auto d-block pt-5"
+        alt=""
+      />
+    </div>
 
+    <!-- Banner end here -->
+
+    <!-- Cart Start here -->
+    <div id="app-display">
+      <a href="../Explore Vehicles/index.html" class="app-text"
+        >Booking Car Now</a
+      >
+
+      <router-link
+        type="button"
+        class="btn btn-primary position-relative float-right"
+        to="/billing"
+      >
+        Cart
+        <span
+          class="cart position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+        >
+          {{ cart }}
+        </span>
+      </router-link>
+    </div>
+
+    <!--1-->
+    <div class="container-fluid bg-explore">
+      <div id="item-list-container">
+        <div id="item-list">
+          <img class="item-image" src="../../assets/img/car-1.png" />
+          <div class="item-title">
+            <h1><b>Mercedes-Benz SLK 200</b></h1>
+          </div>
+          <div class="item-price">
+            <h3>
+              Spesifikasi : <br />
+              - Fuel : Pertamax Turbo <br />- Heat Type : Automatic <br />
+              - Vahicle Class : Sport Car <br />- Seats : 2 Person
+            </h3>
+            <h2>Rp.3.500.000</h2>
+          </div>
+          <button class="item-detail" v-on:click="addToCart">
+            <b>rent now</b>
+          </button>
+          <button class="item-detail-cancel" v-on:click="removeFromCart">
+            <b>cancel</b>
+          </button>
         </div>
+      </div>
+    </div>
 
-        <!-- Banner end here -->
-
-        <!-- Cart Start here -->
-            <div id="app-display">
-        <a href="../Explore Vehicles/index.html" class="app-text">Booking Car Now</a>
-
-                
-                
-                <router-link type="button" class="btn btn-primary position-relative float-right" to="/billing" >
-                    Cart
-                    <span class="cart position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger ">
-                         {{ cart }}
-                    </span>
-                </router-link>
-              
-            </div>
-    
-
- <!--1-->
-            <div class="container-fluid bg-explore">
-                <div id="item-list-container">
-                <div id="item-list">
-                    <img class="item-image" src="../../assets/img/car-1.png">
-                    <div class="item-title">
-                        <h1><b>Mercedes-Benz SLK 200</b></h1>
-                    </div>
-                    <div class="item-price">
-                        <h3>Spesifikasi :
-                            <br> - Fuel : Pertamax Turbo 
-                            <br>- Heat Type : Automatic
-                            <br> - Vahicle Class : Sport Car
-                            <br>- Seats : 2 Person
-                        </h3>
-                        <h2>Rp.3.500.000</h2>
-                    </div>
-                    <button class="item-detail" v-on:click="addToCart"><b>rent now</b></button>
-                    <button class="item-detail-cancel " v-on:click="removeFromCart"><b>cancel</b></button>               
-                </div>
-                </div>
-            </div>
-
-
-
-    
-
-        <!-- Cart end here -->
-
-            
-        
-        
-        </div>
-        
-    
-    
-
+    <!-- Cart end here -->
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/Navbar.vue'
-
-
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-    name: 'Explore',
-        components: {
-    Navbar
-    }
-}
+  name: "Explore",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
