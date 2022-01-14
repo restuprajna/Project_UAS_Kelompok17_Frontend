26ce02b71bb8efb108f197c44d7c052813fae1cc
restuprajna <prajna.restu.rp@gmail.com>
2022-01-14 14:50:12 +0800

    update tampilan footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,44 @@
 <template>
     <footer class="bg-light pt-4">
         <div class="container-fluid py-4" style="background-color: #1687A7;">
-            <div class="row row-cols-3 row-cols-md-3 g-4 mx-5 text-color1" >
-                <div class="col">
-                    <img class="img-fluid ps-3 rounded mx-auto d-block" src="../assets/img/white_car.png" alt="logo" width="170" height="61">
+            <div class="row g-4 mx-5 text-color1" >
+                <div class="col-3">
+                    <img class="img-fluid ps-3 rounded mx-auto d-block float-left" src="../assets/img/white_car.png" alt="logo" width="300" height="300">
+
+                    <p class="font-weight-bold">Let's Find Your Ideal Car 
+                    <br> Enjoy car rental!</p>
                     
                 </div>
-                <div class="col px-4">
-                    <h4 class="fs-4">LOREM IPSUM</h4>
-                        <p class="text-color1 fs-6 fw-normal mt-3">
-                            an agent engaged in luxury car rentals that provides various types of world-famous luxury cars at the lowest prices in Bali.  "Let's find your ideal car"
-                        </p>
+
+                
+
+
+                <div class="col-9 px-4">
+                    
+                        <div class="col-8 pb-5 pt-4 text-light">
+                            <p class="text-color1 fs-6 fw-normal mt-3">
+                                an agent engaged in luxury car rentals that provides various types of world-famous luxury cars at the lowest prices in Bali.  "Let's find your ideal car"
+                            </p>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <img src="../assets/img/phone-icon.png" alt="" height="40" width="40">
+                                <a class="text-decoration-none text-light pl-4" href="https://api.whatsapp.com/send?phone=6289999999">081339961365</a>
+                            </div>
+                            <div class="col">
+                                <img src="../assets/img/facebook-icon.png" alt="" height="40" width="40">
+                                <a href="" class="text-light pl-4">rent car bali</a>
+                            </div>
+                            <div class="col">
+                                <img src="../assets/img/instagram-icon.png" alt="" height="40" width="40">
+                                <a href="" class="text-light pl-4">rent car bali</a>
+                            </div>
+                            
+                            
+                        </div>
                 </div>
-                <div class="col">
+                <!-- <div class="col">
                     <h4 class="fs-4">CONTACT</h4>
                     <ul class="list-unstyled footer-link-list fs-6 mt-3 fw-normal">
                         <li>
@@ -24,10 +50,10 @@
                             <a class="text-decoration-none text-color1" href="mailto:bali@rentcar.com">Email: bali@rentcar.com</a>
                         </li>
                     </ul>
-                </div>
+                </div> -->
             </div>
         <div class="container-fluid mt-5 py-2 fs-6 footer border-top">
-            <p class="pt-3">2021 RentCar | UAS WEB</p>
+            <p class="pt-3 text-light">© 2021 Keenansupercar. All rights reserved.</p>
         </div>
         </div>
     </footer>
--- src/views/customer/Explore.vue
+++ src/views/customer/Explore.vue
@@ -72,17 +72,21 @@
     </div>
 
     <!-- Cart end here -->
+
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Explore",
   components: {
     Navbar,
+    Footer
   },
 };
 </script>
